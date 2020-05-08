ce61cd5cab428c75bd7d7a22265f5136c57294c9
Haewhybabs <babalolaisaac@gmail.com>
2020-05-09 00:34:43 +0100

    uploads folder made public for accessibility

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/uploads', express.static('uploads'));
 
 //Middlewares
 app.use(morgan('dev'));
